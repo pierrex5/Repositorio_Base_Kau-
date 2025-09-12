c0e57d1b1a4ae071a82eac8275943a16cde7ea05
Kauã Pierre <auakpierre@gmail.com>
2025-09-12 16:47:14 -0300

    Add files via upload

---
+++ Modulo 1/Exercicios/teste.py
@@ -0,0 +1,17 @@
+try: 
+    
+   Variavel = int(input("Qual o numero desejado?"))
+   Variavel2 = int(input("Na onde a tabuada irá começar?"))
+   Variavel3 = int(input("E na onde a tabuada irá terminar?"))
+
+   while Variavel2 <= Variavel3:
+      print(f" {Variavel2} x {Variavel} = {Variavel2 * Variavel}")
+      Variavel2 += 1
+
+
+except:
+   print("Ocorreu um erro no número.")
+
+
+
+
