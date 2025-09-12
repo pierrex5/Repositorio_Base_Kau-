6b8209f99a0782e5cf4eec407d329240020a9598
Kauã Pierre <auakpierre@gmail.com>
2025-09-12 16:52:37 -0300

    Add files via upload

---
+++ Modulo 1/Exercicios/teste (3).py
@@ -0,0 +1,16 @@
+try:
+
+
+   Número = int (input ("Primeiro número :"))
+   Número2 = int (input("Segundo número : "))
+
+   print (f" {Número} x {Número2} = {Número * Número2}")
+
+except:
+   print("Erro no número.")
+
+
+
+
+
+
